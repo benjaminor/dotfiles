65fb1560f7a523a1c5fea10ee3169c01042eebf9
ben <benjamin.orthen@bmw.de>
2018-08-10 10:29:55 +0200

    Fix smaller issues, don't use snap, restructure script.

--- deploy_dotfiles.sh
+++ deploy_dotfiles.sh
@@ -8,8 +8,8 @@ set -o errexit -o pipefail -o noclobber -o nounset
 
 ! getopt --test > /dev/null
 if [[ ${PIPESTATUS[0]} -ne 4 ]]; then
-	echo "I’m sorry, `getopt --test` failed in this environment."
-	exit 1
+    echo "I’m sorry, `getopt --test` failed in this environment."
+    exit 1
 fi
 
 OPTIONS=b:f
@@ -21,9 +21,9 @@ LONGOPTS=backup:,force
 # -pass arguments only via   -- "$@"   to separate them correctly
 ! PARSED=$(getopt --options=$OPTIONS --longoptions=$LONGOPTS --name "$0" -- "$@")
 if [[ ${PIPESTATUS[0]} -ne 0 ]]; then
-	# e.g. return value is 1
-	#  then getopt has complained about wrong arguments to stdout
-	exit 2
+    # e.g. return value is 1
+    #  then getopt has complained about wrong arguments to stdout
+    exit 2
 fi
 # read getopt’s output this way to handle the quoting right:
 eval set -- "$PARSED"
@@ -31,45 +31,48 @@ eval set -- "$PARSED"
 f=n backup="$dotfiles"/bak
 # now enjoy the options in order and nicely split until we see --
 while true; do
-	case "$1" in
-		-b|--backup)
-			backup="$2"
-			shift 2
-			;;
-		-f|--force)
-			f=y
-			shift
-			;;
-		--)
-			shift
-			break
-			;;
-		# *)
-		#	echo "Programming error"
-		#	exit 3
-		#	;;
-	esac
+    case "$1" in
+	-b|--backup)
+	    backup="$2"
+	    shift 2
+	    ;;
+	-f|--force)
+	    f=y
+	    shift
+	    ;;
+	--)
+	    shift
+	    break
+	    ;;
+	# *)
+	#       echo "Programming error"
+	#       exit 3
+	#       ;;
+    esac
 done
 
 # handle non-option arguments
 if [[ "$#" -ne 0 ]]; then
-	echo "$0: There should be no input file."
-	exit 4
+    echo "$0: There should be no input file."
+    exit 4
 fi
 
 if [[ "$f" == "n" ]]; then
-	mkdir -p "$backup"
+    mkdir -p "$backup"
 fi
 
 backup_and_symlink(){
-	# argument 1: file to backup
+    # argument 1: file to backup
     # argument 2: file to symlink to
     # TODO: $1 might be a directory
-	if [[ "$f" == "n" && (-f "$1" || -d "$1") ]]; then
-		/bin/mv "$1" "$backup"
-	fi
-	mkdir -p $(dirname "$1")
-	ln -s "$2" "$1"
+    if [[ "$f" == "n" && (-f "$1" || -d "$1") ]]; then
+	/bin/mv "$1" "$backup"
+    fi
+    if [[ "$f" == "y" ]]; then
+	rm -rf "$1"
+    fi
+    mkdir -p $(dirname "$1")
+    ln -s "$2" "$1"
 }
 
 # TODO: add new firefox user.js
--- install_dependencies.sh
+++ install_dependencies.sh
@@ -5,8 +5,8 @@
 # only works with debian based systems
 
 if ! [[ -f "/etc/debian_version" ]]; then
-	echo "Sorry, only works for debian based systems."
-	exit 1
+    echo "Sorry, only works for debian based systems."
+    exit 1
 fi
 
 # saner programming env: these switches turn some bugs into errors
@@ -14,8 +14,8 @@ set -o errexit -o pipefail -o noclobber -o nounset
 
 ! getopt --test > /dev/null
 if [[ ${PIPESTATUS[0]} -ne 4 ]]; then
-	echo "I’m sorry, `getopt --test` failed in this environment."
-	exit 1
+    echo "I’m sorry, `getopt --test` failed in this environment."
+    exit 1
 fi
 
 OPTIONS=i
@@ -27,46 +27,57 @@ LONGOPTS=inquire
 # -pass arguments only via   -- "$@"   to separate them correctly
 ! PARSED=$(getopt --options=$OPTIONS --longoptions=$LONGOPTS --name "$0" -- "$@")
 if [[ ${PIPESTATUS[0]} -ne 0 ]]; then
-	# e.g. return value is 1
-	#  then getopt has complained about wrong arguments to stdout
-	exit 2
+    # e.g. return value is 1
+    #  then getopt has complained about wrong arguments to stdout
+    exit 2
 fi
 # read getopt’s output this way to handle the quoting right:
 eval set -- "$PARSED"
 
 i=n
+timezone="Europe/Berlin"
 # now enjoy the options in order and nicely split until we see --
 while true; do
-	case "$1" in
-		-i|--inquire)
-			i=y
-			shift
-			;;
-		--)
-			shift
-			break
-			;;
-		# *)
-		#	echo "Programming error"
-		#	exit 3
-		#	;;
-	esac
+    case "$1" in
+	-i|--inquire)
+	    i=y
+	    shift
+	    ;;
+	-t|--timezone)
+	    timezone="$2"
+	    shift 2
+	    ;;
+	-h|--help)
+	    echo "Install packages and libraries I usually need on a new debian-based system."
+	    echo "Options -h|--help to show this message"
+	    echo "-i|--inquire to ask for each option"
+	    shift
+	    ;;
+	--)
+	    shift
+	    break
+	    ;;
+	# *)
+	#       echo "Programming error"
+	#       exit 3
+	#       ;;
+    esac
 done
 
 # handle non-option arguments
 if [[ $# -ne 0 ]]; then
-	echo "$0: There should be no input file."
-	exit 4
+    echo "$0: There should be no input file."
+    exit 4
 fi
 
 install_opt="-y"
 if [[ "$i" == "y" ]]; then
-	install_opt=""
+    install_opt=""
 fi
 
 ppa_repos=()
 ppa_libs=()
-snap_libs=()
+# snap_libs=()
 pip_libs=()
 # emacs
 ppa_repos+=("ppa:kelleyk/emacs")
@@ -74,7 +85,6 @@ ppa_libs+=("emacs26")
 
 # keepass2
 ppa_libs+=("keepass2")
-echo "You still have to specify file for keepass2."
 
 # fish shell
 ppa_repos+=("ppa:fish-shell/release-2")
@@ -84,10 +94,11 @@ ppa_libs+=("fish")
 ppa_libs+=("silversearcher-ag")
 
 # firefox
-snap_libs+=("firefox")
+ppa_libs+=("firefox")
 
-# sudo snap install --classic go
-snap_libs+=("--classic go")
+# go
+ppa_repos+=("ppa:gophers/archive")
+ppa_libs+=("golang-1.10-go")
 
 
 ###########################################
@@ -102,44 +113,122 @@ pip_libs+=("codecov")
 pip_libs+=("autopep8")
 pip_libs+=("pycodestyle")
 
+##############################################
+### set up basic settings ####################
+
+echo "$timezone" | sudo tee /etc/timezone
+sudo dpkg-reconfigure --frontend noninteractive tzdata
+
+##############################################
+### install necessary packages ###############
+
+sudo apt-get update
+sudo apt-get install -y software-properties-common
+# sudo apt-get install -y snapd
 
 for repo in "${ppa_repos[@]}"; do
-    sudo add-apt-repository "$repo"
+    sudo add-apt-repository "$install_opt" "$repo"
 done
 
 for lib in "${ppa_libs[@]}"; do
     sudo apt-get install "$install_opt" "$lib"
 done
 
-for lib in "${snap_libs[@]}"; do
-    sudo snap install "$lib"
-done
+# for lib in "${snap_libs[@]}"; do
+#     sudo snap install "$lib"
+# done
 
 # install packages not in debian ppa_repos
 
 #texlive
-temp_files=$dotfiles/tmp
+temp_files="$dotfiles"/tmp
 mkdir -p temp_files
 
+flag_python_installed=0
+
 # conda and anaconda
-# TODO: verify hash 3e58f494ab9fbe12db4460dc152377b5
-echo "Installing Anaconda 5.2. If you want to install a different version, please go to https://docs.anaconda.com/anaconda/install/linux."
-curl -L https://repo.anaconda.com/archive/Anaconda3-5.2.0-Linux-x86_64.sh -o $temp_files/Anaconda3-5.2.0-Linux-x86_64.sh
-bash $temp_files/Anaconda3-5.2.0-Linux-x86_64.sh
+install_anaconda(){
+    echo "Installing Anaconda 5.2. If you want to install a different version, please go to https://docs.anaconda.com/anaconda/install/linux."
+    curl -L https://repo.anaconda.com/archive/Anaconda3-5.2.0-Linux-x86_64.sh -o "$temp_files"/Anaconda3-5.2.0-Linux-x86_64.sh
+
+    if [[ $(sha256sum "$temp_files"/Anaconda3-5.2.0-Linux-x86_64.sh ) -eq "09f53738b0cd3bb96f5b1bac488e5528df9906be2480fe61df40e0e0d19e3d48  Anaconda3-5.2.0-Linux-x86_64.sh" ]]; then
+	bash "$temp_files"/Anaconda3-5.2.0-Linux-x86_64.sh
+	flag_python_installed=1
+    else
+	echo "sha265 checksum of downloaded file not correct"
+    fi
+
+}
+
+install_python(){
+    echo "Installing python3..."
+    sudo apt-get install "$install_opt" python3 python3-pip python3-dev
+    flag_python_installed=1
+}
+
+install_pip_libs(){
+    for lib in "${pip_libs[@]}"; do
+	python -m pip install "$lib"
+    done
+}
 
 # texlive
-rm -r /usr/local/texlive/2018
-rm -r ~/.texlive2018
-mkdir -p $temp_files/texlive
-wget 'http://mirror.ctan.org/systems/texlive/tlnet/install-tl-unx.tar.gz' -P $temp_files
-tar -xzf $temp_files/install-tl-unx.tar.gz -C $temp_files/texlive --strip-components 1
-# TODO: ask if user wants to install texlive
-bash $temp_files/texlive/install-tl
+install_texlive(){
+    rm -r /usr/local/texlive/2018
+    rm -r ~/.texlive2018
+    mkdir -p "$temp_files"/texlive
+    wget 'http://mirror.ctan.org/systems/texlive/tlnet/install-tl-unx.tar.gz' -P "$temp_file"s
+    tar -xzf "$temp_files"/install-tl-unx.tar.gz -C "$temp_files"/texlive --strip-components 1
+    bash "$temp_files"/texlive/install-tl
+}
+
+if [[ "$i" -eq "y" ]]; then
+    echo "Do you wish to install Anaconda 5.2?"
+    select yn in "Yes" "No"; do
+	case "$yn" in
+	    Yes ) install_anaconda; break;;
+	    No ) break;;
+	esac
+    done
+
+
+    if [[ "$flag_python_installed" -eq 0 ]]; then
+	echo "Do you wish to install Python from the official Ubuntu repositories?"
+	select yn in "Yes" "No"; do
+	    case "$yn" in
+		Yes ) install_python; break;;
+		No ) break;;
+	    esac
+	done
+    fi
 
 
 
+    if [[ "$flag_python_installed" -eq 1 ]];then
+	echo "Do you wish to install the preselected list of python libraries from PyPi"
+	select yn in "Yes" "No"; do
+	    case "$yn" in
+		Yes ) install_pip_libs; break;;
+		No ) break;;
+	    esac
+	done
+    fi
 
-rm -rf $temp_files
 
-# vale
-# wget 'https://github.com/errata-ai/vale/releases/download/v0.11.2/vale_0.11.2_Linux_64-bit.tar.gz'
+    echo "Do you wish to install Texlive?"
+    select yn in "Yes" "No"; do
+	case "$yn" in
+	    Yes ) install_texlive; break;;
+	    No ) break;;
+	esac
+    done
+else
+    install_anaconda
+    if [[ "$flag_python_installed" -eq 1 ]]; then
+	install_pip_libs
+    fi
+    install_texlive
+fi
+
+
+rm -rf "$temp_files"
