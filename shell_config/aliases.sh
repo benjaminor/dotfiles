b68287a797fd39263426ea803954351001d37854
Benjamin Orthen <benjamin.orthen@tum.de>
2019-03-06 00:08:42 +0100

    Update emacs subproject, expand aliases for cd etc.

--- shell_config/aliases.sh
+++ shell_config/aliases.sh
@@ -12,7 +12,7 @@ alias l='ls -CF'
 alias emacs='emacs --no-splash'
 alias today='date +"%A, %B %-d, %Y"'
 
-alias uu='sudo apt update && sudo apt upgrade -y  && sudo apt autoremove -y && sudo apt autoclean -y && conda clean -a -y'
+alias uu='sudo apt update && sudo apt upgrade -y  && sudo apt autoremove -y && sudo apt autoclean -y && sudo snap refresh && nix-channel --update && nix-env -u && conda update --all -y && conda clean -a -y'
 alias night='redshift -l 48.15:11.58 &'
 
 ###alias sember='emacs ~/Documents/Latex/Semesterberichte/SemesterberichtSS2016/semesterbericht.tex &'
@@ -49,6 +49,10 @@ alias sudo='sudo ' # enable alias expansion for sudo
 
 alias ..="cd .."
 alias ...="cd ../.."
+alias cd..="cd .."
+alias ...="cd ../.."
+alias ....="cd ../../.."
+alias .....="cd ../../../.."
 
 alias tree="tree -A"
 alias treed="tree -d"
@@ -57,3 +61,9 @@ alias tree2="tree -d -L 2"
 
 alias help="tldr"
 alias jenkins="sudo docker run -u root --rm -d --name my_jenkins -p 8080:8080 -p 50000:50000 -v jenkins-data:/var/jenkins_home -v /var/run/docker.sock:/var/run/docker.sock jenkinsci/blueocean"
+
+# alias for z open directory
+alias zd="z -d"
+
+# networking
+alias wget="curl -O"
