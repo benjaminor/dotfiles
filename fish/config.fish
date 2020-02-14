a05720794a5fcebdecd5e58bbd877b83aadbc511
Benjamin Orthen <benjamin@orthen.net>
2020-02-14 23:07:40 +0100

    Add git plugin for fish, broot,direnv and bash functions support for fish

--- fish/config.fish
+++ fish/config.fish
@@ -11,11 +11,22 @@ end
 if type -q awk
 	set shell_config "$HOME/.config/shell"
 	set functions_file "$shell_config/functions.sh"
-	for line in (awk '/function .*\(\)/ {print substr($2, 1, length($2)-2)}' $functions_file)
-		function $line
-			bass source $functions_file ';' $_ $argv
+	set personal_functions_file "$HOME/.functions_personal"
+	if test -e $functions_file
+		for line in (awk '/function .*\(\)/ {print substr($2, 1, length($2)-2)}' $functions_file)
+			function $line
+				bass source $functions_file ';' $_ $argv
+			end
 		end
 	end
+	if test -e $personal_functions_file
+		for line in (awk '/function .*\(\)/ {print substr($2, 1, length($2)-2)}' $personal_functions_file)
+			function $line
+				bass source $personal_functions_file ';' $_ $argv
+			end
+		end
+	end
+
 end
 
 
@@ -26,3 +37,11 @@ set -g theme_color_scheme solarized-light
 # cht aready defined in functions.sh
 # register completions (on-the-fly, non-cached, because the actual command won't be cached anyway
 complete -c cht -xa '(curl -s cheat.sh/:list)'
+
+# direnv support for fish
+eval (direnv hook fish)
+
+source /home/ben/.config/broot/launcher/fish/br
+
+# opam configuration
+source /home/ben/.opam/opam-init/init.fish > /dev/null 2> /dev/null; or true
