6c26b50c6b76a242dd796e8c507be046ceec92d1
Benjamin Orthen <benjamin@orthen.net>
2020-10-13 18:28:44 +0200

    Add wireguard-tools to arch vps

--- roles/dotfiles/files/nixpkgs/machines/arch-vps/home.nix
+++ roles/dotfiles/files/nixpkgs/machines/arch-vps/home.nix
@@ -1,9 +1,13 @@
+{ pkgs, ... }:
+
 {
   imports = [
     ../../modules/home-manager-basis.nix
     ../../modules/cli.nix
     ../../modules/git.nix
   ];
+
+  home.packages = (with pkgs; [ wireguard-tools ]);
 
   targets.genericLinux.enable = true;
 }
