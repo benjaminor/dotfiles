da49772d7139c18bc7710aaa9efdd15e258a4ac4
Benjamin Orthen <benjamin@orthen.net>
2020-10-25 00:38:34 +0200

    add server-management module with docker-compose

--- roles/dotfiles/files/nixpkgs/machines/arch-vps/home.nix
+++ roles/dotfiles/files/nixpkgs/machines/arch-vps/home.nix
@@ -5,6 +5,7 @@
     ../../modules/home-manager-basis.nix
     ../../modules/cli.nix
     ../../modules/git.nix
+    ../../modules/server-management.nix
   ];
 
   targets.genericLinux.enable = true;
