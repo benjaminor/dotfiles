64edc03aabf1a6a4b8de8aed606a8a3e57c3c3ce
Benjamin Orthen <benjamin@orthen.net>
2020-12-31 19:01:49 +0100

    Add git-secret as package to git submodule

--- roles/dotfiles/files/nixpkgs/modules/git.nix
+++ roles/dotfiles/files/nixpkgs/modules/git.nix
@@ -1,9 +1,10 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
-    programs.git = {
-      enable = true;
-      userName = "Benjamin Orthen";
-      userEmail = "benjamin@orthen.net";
-    };
+  programs.git = {
+    enable = true;
+    userName = "Benjamin Orthen";
+    userEmail = "benjamin@orthen.net";
+  };
+  home.packages = (with pkgs; [ git-secret ]);
 }
